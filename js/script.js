263a7ca32389686a807429e6e0db1515d6ed5bb8
hansveersingh1204 <104035859+hansveersingh1204@users.noreply.github.com>
2022-04-20 02:31:53 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,54 @@
+//switch to dark mode
 function dark_mode() {
+	//variable initialization
 	var elem = document.body;
 	var infoTable = document.getElementsByTagName('table');
 	var themeBtn = document.getElementById('theme-btn');
+	//change body styling
 	elem.style.backgroundColor = "black";
+	elem.style.color = "white";
+	//iterate through all tables
 	for (let i = 0; i < infoTable.length; i++) {
+		//change table styling
 		const table = infoTable[i];
 		table.style.color = "white";
 	}
-	elem.style.color = "white";
+	//change theme button styling 
 	themeBtn.innerHTML = "Light Mode";
 	themeBtn.className = "dark-btn";
 	themeBtn.setAttribute('onclick', 'light_mode()');
 }
 
+//switch to light mode
 function light_mode() {
+	//variable initialization
 	var elem = document.body;
 	var infoTable = document.getElementsByTagName('table');
 	var themeBtn = document.getElementById('theme-btn');
+	//change body styling
 	elem.style.backgroundColor = "#f7f7f7";
+	elem.style.color = "black";
+	//iterate through all tables
 	for (let i = 0; i < infoTable.length; i++) {
+		//change table styling
 		const table = infoTable[i];
 		table.style.color = "black";
 	}
-	elem.style.color = "black";
+	
+	//change theme button styling 
 	themeBtn.innerHTML = "Dark Mode";
 	themeBtn.className = "light-btn";
 	themeBtn.setAttribute('onclick', 'dark_mode()');
 }
 
+//if scroll, call function stick()
 window.onscroll = function() {stick()};
+//variable initialization
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-
-function stick(){
-	navbarHeight = window.getComputedStyle(navbar, null).getPropertyValue('height');
+//add or remove sticky class depending on page offset
+function stick() {
 	if (window.pageYOffset > sticky) {
 		navbar.classList.add("sticky");
 	}
